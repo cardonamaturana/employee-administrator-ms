ed441b8cc306425c26f49bedcd8ed23d8eaf4a19
CardonaDeveloperZabud <julio.cardona@zabud.com.co>
2023-11-02 17:43:31 -0500

    initial commit

---
+++ src/main/java/com/cardonamaturana/employeeadministratorms/EmployeeAdministratorMsApplication.java
@@ -0,0 +1,13 @@
+package com.cardonamaturana.employeeadministratorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAdministratorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAdministratorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardonamaturana/employeeadministratorms/EmployeeAdministratorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardonamaturana.employeeadministratorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeAdministratorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
